7d520114772125cd872fab31e5934ec01482052b
mzee-flutter <mudair.khan37@gmail.com>
2025-01-16 11:17:19 +0400

    main updates

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: PayPage.id,
+      initialRoute: PlantDetailPage.id,
       routes: {
         LoginPage.id: (context) => LoginPage(),
         ProductPage.id: (context) => ProductPage(),
